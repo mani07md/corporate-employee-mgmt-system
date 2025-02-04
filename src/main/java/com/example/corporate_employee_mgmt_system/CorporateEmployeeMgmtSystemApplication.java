851fe05cb4a9b73c2a3888b4bd2c26d80a204611
manikanta velpuri <manikantavelpuri97@gmail.com>
2025-02-04 15:51:28 +0530

    created project in spring boot

---
+++ src/main/java/com/example/corporate_employee_mgmt_system/CorporateEmployeeMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.corporate_employee_mgmt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateEmployeeMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateEmployeeMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corporate_employee_mgmt_system/CorporateEmployeeMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corporate_employee_mgmt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateEmployeeMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
