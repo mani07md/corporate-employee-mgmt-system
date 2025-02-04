e584b40cc83ebefc07b6c3e32ed0491d06cbb6ab
manikanta velpuri <manikantavelpuri97@gmail.com>
2025-02-04 16:08:27 +0530

    new command lines are added

--- src/main/java/com/example/corporate_employee_mgmt_system/CorporateEmployeeMgmtSystemApplication.java
+++ src/main/java/com/example/corporate_employee_mgmt_system/CorporateEmployeeMgmtSystemApplication.java
@@ -8,6 +8,10 @@ public class CorporateEmployeeMgmtSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CorporateEmployeeMgmtSystemApplication.class, args);
+		/* new program 
+		 * manikanta velpuri
+		 * text commands for project
+		 */
 	}
 
 }
